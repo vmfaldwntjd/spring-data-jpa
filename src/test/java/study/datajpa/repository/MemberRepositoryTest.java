8c8b6c9247ac9acd3d1a8f8f8355dee9c6e8da2c
core <jinjs6@gmail.com>
2023-03-24 18:27:44 +0900

    test: jpa 기본 페이징 테스트

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -13,6 +13,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -143,5 +144,17 @@ class MemberRepositoryTest {
         assertThat(result.get(0).getAge()).isEqualTo(m1.getAge());
         assertThat(result.get(1).getUsername()).isEqualTo(m2.getUsername());
         assertThat(result.get(1).getAge()).isEqualTo(m2.getAge());
+    }
+
+    @Test
+    public void returnType() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> aaa = memberRepository.findListByUsername("AAA");
+        assertThat(aaa.size()).isEqualTo(1);
+        assertThat(aaa.get(0).getUsername()).isEqualTo(m1.getUsername());
     }
 }
