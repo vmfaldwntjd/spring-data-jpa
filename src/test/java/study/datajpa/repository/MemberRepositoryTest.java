a509af871aa7efa103feab615475afd80f263c1b
core <jinjs6@gmail.com>
2023-03-25 16:37:29 +0900

    test: spring data jpa custom interface 실습

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -263,4 +263,9 @@ class MemberRepositoryTest {
         //when
         List<Member> result = memberRepository.findLockByUsername("member1");
     }
+
+    @Test
+    public void callCustom() {
+        List<Member> result = memberRepository.findMemberCustom();
+    }
 }
